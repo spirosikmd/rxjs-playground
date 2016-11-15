d51f0df079a475a219b4306af296f7b7ed3dfe0c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-11-16 00:14:02 +0100

    Use merge

--- app.ts
+++ app.ts
@@ -1,21 +1,32 @@
 import {Observable} from 'rxjs/Rx';
 
+interface Data {
+  count: number;
+}
+
 const startButton = document.querySelector('#start');
 const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
 
 const start$ = Observable.fromEvent(startButton, 'click');
-const interval$ = Observable.interval(1000);
 const stop$ = Observable.fromEvent(stopButton, 'click');
+const reset$ = Observable.fromEvent(resetButton, 'click');
+const interval$ = Observable.interval(1000);
+
+const data: Data = {count: 0};
+const inc = (acc: Data): Data => ({count: acc.count + 1});
+const reset = (acc: Data): Data => data;
 
 const intervalThatStops$ = interval$
   .takeUntil(stop$);
 
-const data = 0;
+const incOrReset$ = Observable.merge(
+  intervalThatStops$.mapTo(inc),
+  reset$.mapTo(reset)
+);
 
 start$
-  .switchMapTo(intervalThatStops$)
+  .switchMapTo(incOrReset$)
   .startWith(data)
-  .scan((acc) => {
-    return acc + 1;
-  })
+  .scan((acc, curr: (acc: Data) => Data) => curr(acc))
   .subscribe((x) => console.log(x));
