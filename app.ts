ba22e557ae4dac569de4a741f9b90db47be65867
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2016-11-16 22:24:47 +0100

    Refactoring composable streams in RxJS

--- app.ts
+++ app.ts
@@ -5,10 +5,16 @@ interface Data {
 }
 
 const startButton = document.querySelector('#start');
+const halfButton = document.querySelector('#half');
+const quarterButton = document.querySelector('#quarter');
+
 const stopButton = document.querySelector('#stop');
 const resetButton = document.querySelector('#reset');
 
 const start$ = Observable.fromEvent(startButton, 'click');
+const half$ = Observable.fromEvent(halfButton, 'click');
+const quarter$ = Observable.fromEvent(quarterButton, 'click');
+
 const stop$ = Observable.fromEvent(stopButton, 'click');
 const reset$ = Observable.fromEvent(resetButton, 'click');
 const interval$ = Observable.interval(1000);
@@ -25,8 +31,21 @@ const incOrReset$ = Observable.merge(
   reset$.mapTo(reset)
 );
 
-start$
-  .switchMapTo(incOrReset$)
+const starters$ = Observable.merge(
+  start$.mapTo(1000),
+  half$.mapTo(500),
+  quarter$.mapTo(250)
+);
+
+const intervalActions = (time: number) => Observable.merge(
+  Observable.interval(time)
+    .takeUntil(stop$)
+    .mapTo(inc),
+  reset$.mapTo(reset)
+);
+
+starters$
+  .switchMap(intervalActions)
   .startWith(data)
   .scan((acc, curr: (acc: Data) => Data) => curr(acc))
   .subscribe((x) => console.log(x));
